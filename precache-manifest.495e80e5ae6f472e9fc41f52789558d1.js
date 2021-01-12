e27ac7d2b6d229dc36192f9ea80676cde8f558ec
CPMSP <cpetersonmsp@gmail.com>
2021-01-11 23:46:29 -0600

    Updates

---
+++ precache-manifest.495e80e5ae6f472e9fc41f52789558d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed29de3310b23bd931974ee350ab478",
+    "url": "/reactlightsout/index.html"
+  },
+  {
+    "revision": "9ed00ce74bd834033c60",
+    "url": "/reactlightsout/static/css/main.10ad0b30.chunk.css"
+  },
+  {
+    "revision": "24f51adae503bd10f57d",
+    "url": "/reactlightsout/static/js/2.76ceaabf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactlightsout/static/js/2.76ceaabf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed00ce74bd834033c60",
+    "url": "/reactlightsout/static/js/main.c5ebadf7.chunk.js"
+  },
+  {
+    "revision": "db279d0de95cdaed8ae3",
+    "url": "/reactlightsout/static/js/runtime-main.6165a459.js"
+  }
+]);
